8dfce1997d1dbef1fb1ad210e99c16fabaabfaf8
ANGUSsugna <90929609+ANGUSsugna@users.noreply.github.com>
2021-10-09 21:34:38 -0400

    Update library.h

--- library.h
+++ library.h
@@ -1,12 +1,4 @@
-
-
-#include "sofaWall.h"
-#include "twoDoors.h"
-#include "fourthWall.h"
-  #include "Book.h"
-
-
-void library(){
+const int Library(){
 
 
 display.clearDisplay(); 
@@ -26,16 +18,12 @@ display.drawRect(12, 10, 25, 37, 1);    //bookcase 1
     display.drawRect(16, 12, 3, 6, 1); 
     display.drawRect(19, 12, 3, 6, 1); 
     display.drawRect(21, 12, 3, 6, 1); 
-
-
-
      
   display.drawLine(12, 28, 35, 28, 1);   //Shelf 2 
     display.drawRect(13, 20, 4, 9, 1);     //book
     display.drawRect(17, 20, 3, 9, 1); //book
     display.drawRect(20, 20, 4, 9, 1); //book
     display.drawRect(24, 22, 3, 7, 1); //book
-
  
   display.drawLine(12, 38, 35, 38, 1);   //Shelf 3 
     display.drawRect(13, 30, 3, 8, 1); //book
@@ -65,8 +53,6 @@ display.drawLine(40, 18, 63, 18, 1);   //Shelf 1
     display.drawRect(44, 22, 3, 7, 1); //book
     display.drawRect(47, 22, 3, 7, 1); //book
     display.drawTriangle(52,28, 55,21, 58,28, 1); //triangle
-
-  
     
   display.drawLine(40, 38, 63, 38, 1);   //Shelf 3
   
@@ -81,7 +67,6 @@ display.drawLine (93,17, 93,22, 1);
 display.drawLine (93,22, 102,13, 1);
 display.drawRect (70,43, 10, 3, 1); //base 
 
-
 //// Lounger
 
 display.drawRoundRect(90, 25, 20, 13, 5, 1);          //sofa back
@@ -94,24 +79,5 @@ display.drawRect(110, 28, 3, 18, 1);                    //right arm
 //display.drawRect(96, 32, 3, 5, 0);                 //doorknob
 
 display.display();
-
-if (upButtonState == LOW){             
-                     Book();                                          
-                           
-                     } else if (downButtonState == LOW){             
-                          sofaWall();                             
-                          
-                     } else if (leftButtonState == LOW){                 
-                          library();
-                         
-                     } else if (rightButtonState == LOW){
-                           fourthWall();
-                     }   
-
-
-
-
-
-
 
 }
